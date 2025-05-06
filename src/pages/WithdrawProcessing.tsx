721ecfef5ed6e853471ef4d59703aa5ed20fbc60
Linerty17 <sundaychinemerem66@gmail.com>
2025-05-06 14:41:21 +0100

    Update WithdrawProcessing.tsx

--- src/pages/WithdrawProcessing.tsx
+++ src/pages/WithdrawProcessing.tsx
@@ -76,7 +76,7 @@ const WithdrawProcessing = () => {
         
         <h1 className="text-3xl font-bold mb-4 text-center">Processing your request</h1>
         <p className="text-lg text-gray-500 text-center">
-          Please wait while we prepare your payment details...
+          Please wait while we credit your account details...
         </p>
       </div>
     </div>
